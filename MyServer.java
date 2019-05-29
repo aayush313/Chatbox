f365542e72c32e8fd201b7a65952bf31323c6aeb
aayush313 <35486320+aayush313@users.noreply.github.com>
2019-05-30 01:00:23 +0530

    Add files via upload

---
+++ MyServer.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class MyServer
+{
+	ArrayList al=new ArrayList();
+	ArrayList users=new ArrayList();
+	ServerSocket ss;
+	Socket s;
+
+	public final static int PORT=10;
+	public final static String UPDATE_USERS="updateuserslist:";
+	public final static String LOGOUT_MESSAGE="@@logoutme@@:";
+	public MyServer()
+	{
+		try{
+			ss=new ServerSocket(PORT);
+			System.out.println("Server Started "+ss);
+			while(true)
+			{
+				s=ss.accept();
+				Runnable r=(Runnable) new MyThread(s,al,users);
+				Thread t=new Thread(r);
+				t.start();
+//	System.out.println("Total alive clients : "+ss.);
+			}
+		}
+		catch(Exception e)
+		{
+			System.err.println("Server constructor"+e);
+		}
+		}
+	
+	public static void main(String [] args)
+	{
+		new MyServer();
+	}
+}
+
+
+
